300b7cf8d0c86a19f0ee950e9f39f6dc38ca5968
JS Báez <sbaezt1200@alumno.ipn.mx>
2018-08-31 19:11:57 -0500

    primeros cambios

---
+++ arrays_asociativo.php
@@ -0,0 +1,9 @@
+<?php
+
+  $alex = array('telefono' => 65487987, 'edad' => 25, 'pais' => 'México');
+
+  echo $alex['edad'].'<br />';
+  echo $alex['pais'].'<br />';
+  echo $alex['telefono'].'<br />';
+
+ ?>
